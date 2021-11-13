7e782f03b157ed684a7db23e227a3bbbe39604b1
Jonas Isensee <jonas.isensee@ds.mpg.de>
2021-11-13 12:46:38 +0100

    use jll package instead

--- src/Gmsh.jl
+++ src/Gmsh.jl
@@ -1,18 +1,42 @@
+# Copied from https://github.com/gridap/GridapGmsh.jl/blob/master/deps/build.jl
+# to use the new officially registered Gmsh JLL
+
 module Gmsh
 
-const deps_path = abspath(joinpath(dirname(@__FILE__), "..", "deps"))
-
-if !isfile(joinpath(deps_path, "deps.jl"))
-    error("Gmsh not installed properly, run Pkg.build(\"Gmsh\"), restart Julia and try again")
-end
-include(joinpath(deps_path, "deps.jl"))
-include(joinpath(deps_path, "usr", "lib", "gmsh.jl"))
-global const api = gmsh
-
-function __init__()
-    # Always check your dependencies that live in `deps.jl`
-    check_deps()
+deps_jl = joinpath(@__DIR__, "..", "deps", "deps.jl")
+if !isfile(deps_jl)
+  s = """
+  Package GridapGmsh not installed properly.
+  Run Pkg.build(\"GridapGmsh\"), restart Julia and try again.
+  """
+  error(s)
 end
 
+include(deps_jl)
 
+if GMSH_FOUND
+    include(gmsh_jl)
+    # Hack taken from MPI.jl
+    function __init__()
+      @static if Sys.isunix()
+        Libdl.dlopen(gmsh.lib, Libdl.RTLD_LAZY | Libdl.RTLD_GLOBAL)
+      end
+    end
+  else
+      s = """
+      Gmsh not found in system paths.
+      Install Gmsh or export path to Gmsh and rebuild the project.
+      Run Pkg.build(\"GridapGmsh\"), restart Julia and try again.
+      """
+      @warn s
+  end
+  
+  macro check_if_loaded()
+    quote
+      if ! GMSH_FOUND
+        error("GridapGmsh is not loaded or installed properly.")
+      end
+    end
+  end
+  
 end # module
