da6213f31ae6969c2113c9b893131b550b5c5542
Sebastian Grimberg <sjg@amazon.com>
2022-05-11 11:21:59 -0700

    Use JLL package for Gmsh

--- src/Gmsh.jl
+++ src/Gmsh.jl
@@ -1,18 +1,8 @@
 module Gmsh
+using Reexport
 
-const deps_path = abspath(joinpath(dirname(@__FILE__), "..", "deps"))
+import gmsh_jll
+include(gmsh_jll.gmsh_api)
+@reexport import .gmsh
 
-if !isfile(joinpath(deps_path, "deps.jl"))
-    error("Gmsh not installed properly, run Pkg.build(\"Gmsh\"), restart Julia and try again")
 end
-include(joinpath(deps_path, "deps.jl"))
-include(joinpath(deps_path, "usr", "lib", "gmsh.jl"))
-global const api = gmsh
-
-function __init__()
-    # Always check your dependencies that live in `deps.jl`
-    check_deps()
-end
-
-
-end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,15 @@
-using Test, Gmsh
+using Test
 
-@testset "test_t1.jl" begin include("test_t1.jl") end
+const path = mktempdir()
+
+@testset "t1.jl" begin
+	include("test_t1.jl")
+	@test filesize(joinpath(path, "t1.msh")) > 0
+end
+
+@testset "t16.jl" begin
+	include("test_t16.jl")
+	@test filesize(joinpath(path, "t16.msh")) > 0
+end
+
+rm(path, recursive=true)
