f12e7105896640ace3ef80692ce376b6b1d81524
Fredrik Ekre <ekrefredrik@gmail.com>
2022-09-24 15:13:58 +0200

    Add wrappers around gmsh.initialize and gmsh.finalize.

--- src/Gmsh.jl
+++ src/Gmsh.jl
@@ -5,4 +5,35 @@ import gmsh_jll
 include(gmsh_jll.gmsh_api)
 @reexport import .gmsh
 
+"""
+    Gmsh.initialize(; finalize_atexit=true)
+
+Wrapper around `gmsh.initialize` which make sure to only call it if `gmsh` is not already
+initialized. Return `true` if `gmsh.initialize` was called, and `false` if `gmsh` was
+already initialized.
+
+If `finalize_atexit` is `true` a Julia exit hook is added, which calls `finalize()`.
+"""
+function initialize(; finalize_atexit=true)
+    if Bool(gmsh.isInitialized())
+        return false
+    end
+    gmsh.initialize()
+    if finalize_atexit
+        atexit(finalize)
+    end
+    return true
+end
+
+"""
+    Gmsh.finalize()
+
+Wrapper around `gmsh.finalize` which make sure to only call it if `gmsh` is initialized.
+"""
+function finalize()
+    if Bool(gmsh.isInitialized())
+        gmsh.finalize()
+    end
+end
+
 end
