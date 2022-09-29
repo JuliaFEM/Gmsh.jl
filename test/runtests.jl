a86f4f144428d806259daef8a6ce33c58df84959
Fredrik Ekre <ekrefredrik@gmail.com>
2022-09-29 14:40:15 +0200

    Pass through argument vector from Gmsh.initialize to gmsh.initialize.

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,3 +13,26 @@ end
 end
 
 rm(path, recursive=true)
+
+@testset "Gmsh.(initialize|finalize) wrappers" begin
+    Gmsh.finalize()
+    # With exit hook
+    l = length(Base.atexit_hooks)
+    @test !Bool(gmsh.isInitialized())
+    @test Gmsh.initialize()
+    @test Bool(gmsh.isInitialized())
+    @test length(Base.atexit_hooks) == l + 1
+    Gmsh.finalize()
+    # Without exit hook
+    l = length(Base.atexit_hooks)
+    @test !Bool(gmsh.isInitialized())
+    @test Gmsh.initialize(; finalize_atexit=false)
+    @test Bool(gmsh.isInitialized())
+    @test length(Base.atexit_hooks) == l
+    Gmsh.finalize()
+    # argv
+    @test Gmsh.initialize(["gmsh", "-v", "0"])
+    @test Bool(gmsh.isInitialized())
+    @test gmsh.option.getNumber("General.Verbosity") == 0
+    Gmsh.finalize()
+end
