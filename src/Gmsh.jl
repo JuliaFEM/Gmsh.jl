b2ff2e2ef75cf4a51541d6c8b980a9f908b0f12e
Fredrik Ekre <ekrefredrik@gmail.com>
2022-09-29 14:40:15 +0200

    Pass through argument vector from Gmsh.initialize to gmsh.initialize.

--- src/Gmsh.jl
+++ src/Gmsh.jl
@@ -6,19 +6,23 @@ import .gmsh
 export gmsh
 
 """
-    Gmsh.initialize(; finalize_atexit=true)
+    Gmsh.initialize(argv=String[]; finalize_atexit=true)
 
 Wrapper around `gmsh.initialize` which make sure to only call it if `gmsh` is not already
 initialized. Return `true` if `gmsh.initialize` was called, and `false` if `gmsh` was
 already initialized.
 
+The argument vector `argv` is passed to `gmsh.initialize`. `argv` can be used to pass
+command line options to Gmsh, see [Gmsh documentation for more
+details](https://gmsh.info/doc/texinfo/gmsh.html#index-Command_002dline-options).
+
 If `finalize_atexit` is `true` a Julia exit hook is added, which calls `finalize()`.
 """
-function initialize(; finalize_atexit=true)
+function initialize(argv=String[]; finalize_atexit=true)
     if Bool(gmsh.isInitialized())
         return false
     end
-    gmsh.initialize()
+    gmsh.initialize(argv)
     if finalize_atexit
         atexit(finalize)
     end
